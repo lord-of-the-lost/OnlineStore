cfc65a64c65ea9af816804daca7eecc2e0bac764
EvgeniyPoleshchuk <123636823+EvgeniyPoleshchuk@users.noreply.github.com>
2024-05-03 12:50:44 +0300

    addImageCategoryAndFavoriteScreen

--- app/src/main/java/com/example/onlinestore/views/HomeScreen/ProductViewModelTest.kt
+++ app/src/main/java/com/example/onlinestore/views/HomeScreen/ProductViewModelTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.onlinestore.views.HomeScreen.network.Categories
 import com.example.onlinestore.views.HomeScreen.network.Products
 import com.example.onlinestore.views.HomeScreen.network.productService
 import kotlinx.coroutines.launch
@@ -12,23 +13,26 @@ import java.lang.Exception
 class ProductViewModelTest : ViewModel() {
 
     private val _productState = mutableStateOf(ProductState())
-    val productState:State<ProductState> = _productState
+    val productState: State<ProductState> = _productState
+    private val _categoryState = mutableStateOf(CategoryState())
+    val categoryState: State<CategoryState> = _categoryState
 
     init {
-     fetch()
-   }
+        fetchProducts()
+        fetchCategories()
+    }
 
-   private fun fetch(){
+    private fun fetchProducts() {
         viewModelScope.launch {
             try {
                 val response = productService.getAllProduct()
-                 _productState.value = _productState.value.copy(
+                _productState.value = _productState.value.copy(
                     list = response,
                     loading = false,
                     error = null
                 )
 
-            }catch (e:Exception){
+            } catch (e: Exception) {
                 _productState.value = _productState.value.copy(
                     loading = false,
                     error = "Error"
@@ -36,6 +40,27 @@ class ProductViewModelTest : ViewModel() {
             }
         }
     }
+
+    private fun fetchCategories() {
+        viewModelScope.launch {
+            try {
+                val response = productService.getAllCategories()
+                _categoryState.value = _categoryState.value.copy(
+                    list = response,
+                    loading = false,
+                    error = null
+                )
+
+            } catch (e: Exception) {
+                _productState.value = _productState.value.copy(
+                    loading = false,
+                    error = "Error"
+                )
+            }
+        }
+    }
+
+
     data class ProductState(
         var loading: Boolean = true,
         var list: Products? = null,
@@ -43,5 +68,11 @@ class ProductViewModelTest : ViewModel() {
 
     )
 
+    data class CategoryState(
+        var loading: Boolean = true,
+        var list: Categories? = null,
+        var error: String? = null
+
+    )
 
 }
