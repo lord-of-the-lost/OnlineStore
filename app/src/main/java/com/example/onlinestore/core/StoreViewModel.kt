ebd71121948acb9da102038418ab801ad1c09019
EvgeniyPoleshchuk <123636823+EvgeniyPoleshchuk@users.noreply.github.com>
2024-05-05 13:37:14 +0300

    fixSearchBar

--- app/src/main/java/com/example/onlinestore/core/StoreViewModel.kt
+++ app/src/main/java/com/example/onlinestore/core/StoreViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.onlinestore.core
 
 import android.app.Application
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.room.Room
@@ -66,6 +68,9 @@ class StoreViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _categories = MutableStateFlow<List<CategoryModel>>(emptyList())
     val categories: StateFlow<List<CategoryModel>> = _categories.asStateFlow()
+
+    val search:MutableState<String> = mutableStateOf("")
+    val search2 = mutableStateOf("")
 
     init {
         loadProducts()
@@ -254,7 +259,10 @@ class StoreViewModel(application: Application) : AndroidViewModel(application) {
     fun sortProductsByTitleDescending() {
         _products.value = _products.value.sortedByDescending { it.title }
     }
-}
+    fun sortByName(){
+        _products.value = _products.value.sortedByDescending { it.title .startsWith("Classic") } }
+    }
+
 
 data class AuthState(
     val success: Boolean = false,
